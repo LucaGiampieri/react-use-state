21361274c380a34a6c01192da2765e0c5b7d06ab
Luca Giampieri <luca.giampieri95@gmail.com>
2026-01-21 15:00:33 +0100

    Fine settaggio e aggiunta MainHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import MainHeader from './MainHeader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <MainHeader />
     </>
   )
 }
---
+++ src/MainHeader.jsx
@@ -0,0 +1,9 @@
+function MainHeader() {
+    return (
+        <>
+            <h1 className="header-title">Learn Web development</h1>
+        </>
+    )
+}
+
+export default MainHeader
